c4087eddc60f8810ff9c3b2a55422ccf529d811d
Beaglefoot <ch-e-st@yandex.ru>
2017-12-27 22:58:48 +0300

    Create and render todos with mobx and mst

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,33 +1,49 @@
 import React from 'react';
 import DevTools from 'mobx-react-devtools';
-import cuid from 'cuid';
-// import { reaction } from 'mobx';
 
-import TodoStore from 'src/models/TodoStoreMST';
-import TodoItem from 'src/models/TodoItemMST';
 import TodoList from 'src/components/TodoList/TodoList';
 import { app } from './App.scss';
 
-console.time('mst');
-const items = new Array(5000).fill().map((_, i) => TodoItem.create({ value: `no: ${i}` }));
-const store = TodoStore.create({
-  list: items
-});
-console.timeEnd('mst');
+// MST
+import TodoStoreMST from 'src/models/TodoStoreMST';
+import TodoItemMST from 'src/models/TodoItemMST';
 
-// const savedStoreState = JSON.parse(localStorage.getItem('TodoStore'));
-// if (savedStoreState) store.replaceList(savedStoreState);
-//
-// reaction(
-//     () => JSON.stringify(store.list.toJS()),
-//     data => localStorage.setItem('TodoStore', data)
-// );
+console.time('MST: generate items');
+const itemsMST = new Array(5000).fill().map((_, i) => TodoItemMST.create({ value: `no: ${i}` }));
+console.timeEnd('MST: generate items');
+
+console.time('MST: create store');
+const storeMST = TodoStoreMST.create({
+  list: itemsMST
+});
+console.timeEnd('MST: create store');
+
+// MobX
+import TodoStore from 'src/models/TodoStore';
+import TodoItem from 'src/models/TodoItem';
+
+console.time('MobX: generate items');
+const items = new Array(5000).fill().map((_, i) => new TodoItem({ value: `no: ${i}` }));
+console.timeEnd('MobX: generate items');
+
+console.time('MobX: create store');
+const store = new TodoStore(items);
+console.timeEnd('MobX: create store');
+
 
 
 const App = () => (
-  <div className={app}>
-    <h1>Todo List</h1>
-    <TodoList store={store} />
+  <div>
+    <div className={app}>
+      <div>
+        <h1>MST Todo List</h1>
+        <TodoList store={storeMST} />
+      </div>
+      <div>
+        <h1>MobX Todo List</h1>
+        <TodoList store={store} />
+      </div>
+    </div>
     <DevTools />
   </div>
 );
