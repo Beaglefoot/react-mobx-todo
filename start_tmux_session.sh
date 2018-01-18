27b8dcfd1789e6c3b225a1707f16dc76ca8ff633
Beaglefoot <ch-e-st@yandex.ru>
2018-01-18 12:07:45 +0300

    Update tmux config

--- start_tmux_session.sh
+++ start_tmux_session.sh
@@ -4,12 +4,13 @@ WORKING_DIR="$PWD"
 
 tmux new-session -d -s $SESSION_NAME
 tmux rename-window $WINDOW_NAME
-tmux send-keys "cd $WORKING_DIR; webpack-dev-server" C-m
+tmux send-keys "nodemon express.js -w index.html -w dist" C-m
 
-tmux split-window -v
-# tmux send-keys "cd $WORKING_DIR; explorer ." C-m
+tmux split-window -h
 
-# tmux split-window -h -t {top}
-# tmux send-keys "cd $WORKING_DIR; ./run_test_watch.sh" C-m
+tmux split-window -v -t {bottom}
+tmux send-keys "webpack --config webpack.prod.config.js -w" C-m
+
+tmux select-pane -t {right}
 
 tmux attach -t $SESSION_NAME
